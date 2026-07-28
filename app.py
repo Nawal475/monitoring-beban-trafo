77d139838d57b9af9f4b593806d03e63d83a0c4f
Nawal Putri <nawalputri@Nawals-MacBook-Pro.local>
2026-07-28 10:25:47 +0700

    prepare railway deployment

--- app.py
+++ app.py
@@ -21,10 +21,7 @@ def inject_user():
 # KONEKSI NEON
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
+        os.getenv("DATABASE_URL"),
         sslmode="require"
     )
 
